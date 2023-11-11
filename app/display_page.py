8ce6108338daa459debf64fd905ee32934a4cb25
oliverbrace <42202976+oliverbrace@users.noreply.github.com>
2023-11-11 20:01:11 +0000

    Fix sort add summary

--- app/display_page.py
+++ app/display_page.py
@@ -11,24 +11,43 @@ class DisplayPage(MDScreen):
     def __init__(self, **kwargs):
         super(DisplayPage, self).__init__(**kwargs)
         self.page_banner = PageBanner("Display Page", self.go_to_welcome_page)
+        self.summary_table = MDDataTable(
+            background_color_selected_cell=table_colour,
+            check=False,
+            height=120,
+            size_hint=(1, None),
+            column_data=[
+                ("Total Cards", dp(18)),
+                ("Uq Cards", dp(15)),
+                ("Total 1st Ed", dp(18)),
+                ("Total Min Price", dp(30)),
+                ("Total Avg Price", dp(35)),
+                ("Total Max Price", dp(35)),
+            ],
+        )
+
         self.data_tables = MDDataTable(
             background_color_selected_cell=table_colour,
             check=True,
+            use_pagination=True,
+            # Not possible to sort first column
             column_data=[
-                ("Name", dp(40), self.sort_on_col_1),
-                ("Rarity", dp(30), self.sort_on_col_2),
+                ("No.", dp(20)),
+                ("Name", dp(40), self.sort_name),
+                ("Rarity", dp(30), self.sort_rarity),
                 ("print_tag", dp(30)),
-                ("1st Ed.", dp(30), self.sort_on_col_3),
-                ("Dmged", dp(30), self.sort_on_col_4),
-                ("Min Price", dp(25), self.sort_on_col_5),
-                ("Avg Price", dp(25), self.sort_on_col_6),
-                ("Max Price", dp(25), self.sort_on_col_7),
+                ("1st Ed.", dp(30), self.sort_1st_ed),
+                ("Dmged", dp(30), self.sort_dmged),
+                ("Min Price", dp(25), self.sort_min_price),
+                ("Avg Price", dp(25), self.sort_avg_price),
+                ("Max Price", dp(25), self.sort_max_price),
                 ("Notes", dp(60)),
             ],
         )
 
         page_content = MDBoxLayout(orientation="vertical")
         page_content.add_widget(self.page_banner)
+        page_content.add_widget(self.summary_table)
         page_content.add_widget(self.data_tables)
         self.add_widget(page_content)
 
@@ -36,31 +55,109 @@ class DisplayPage(MDScreen):
         self.manager.transition.direction = "right"
         self.manager.current = "welcome_page"
 
+    @staticmethod
+    def string_prices_to_numbers(prices):
+        """Takes a series of string numbers and returns a dataframe of those numbers
+
+        Returns:
+            _type_: _description_
+        """
+        split_prices = prices.str.split("-", expand=True)
+        if len(split_prices.columns) == 1:
+            split_prices[1] = split_prices[0]
+        else:
+            split_prices[1] = split_prices[1].fillna(split_prices[0])
+        split_prices[0] = pd.to_numeric(split_prices[0].str.replace("£", ""))
+        split_prices[1] = pd.to_numeric(split_prices[1].str.replace("£", ""))
+        return split_prices
+
+    def calculate_total_price(self, prices):
+        """_summary_
+
+        Args:
+            prices (pd.Series): series of prices of cards. Can either be a
+                number or range like "x-y"
+        """
+        split_prices = self.string_prices_to_numbers(prices)
+        low_total = split_prices[0].sum()
+        high_total = split_prices[1].sum()
+
+        if low_total == high_total:
+            return f"£{split_prices[0].sum():.2f}"
+
+        return f"£{split_prices[0].sum():.2f}-£{split_prices[1].sum():.2f}"
+
+    @staticmethod
+    def table_format(data):
+        """Put pandas data in format for table
+
+        Args:
+            data (pd.DataFrame): data to be turned into put into a table
+
+        Returns:
+            _type_: _description_
+        """
+        return list(data.itertuples(index=False, name=None))
+
     def on_pre_enter(self):
-        pd_cards = pd.read_csv("card_data.csv").fillna("")
-        cards = list(pd_cards.itertuples(index=False, name=None))
+        pd_cards = pd.read_csv("card_data.csv").reset_index().fillna("")
+        cards = self.table_format(pd_cards)
         self.data_tables.row_data = cards  # Populate the MDDataTable
+        if len(cards) == 0:
+            self.summary_table.row_data = [("-", "-", "-", "-", "-", "-")]
+        else:
+            self.summary_table.row_data = [
+                (
+                    len(pd_cards),
+                    len(pd.unique(pd_cards["card_name"])),
+                    pd_cards["first_edition"].sum(),
+                    self.calculate_total_price(pd_cards["lowest_card_price"]),
+                    self.calculate_total_price(pd_cards["average_card_price"]),
+                    self.calculate_total_price(pd_cards["highest_card_price"]),
+                )
+            ]
 
-    def sort_on_col_1(self, data):
+    def sort_name(self, data):
         return zip(*sorted(enumerate(data), key=lambda l: l[1][1]))
 
-    def sort_on_col_2(self, data):
-        return zip(*sorted(enumerate(data), key=lambda l: l[1][1]))
+    def sort_rarity(self, data):
+        return zip(*sorted(enumerate(data), key=lambda l: l[1][2]))
 
-    def sort_on_col_3(self, data):
-        return zip(*sorted(enumerate(data), key=lambda l: l[1][3]))
-
-    def sort_on_col_4(self, data):
-        return zip(*sorted(enumerate(data), key=lambda l: l[1][3]))
-
-    def sort_on_col_5(self, data):
+    def sort_1st_ed(self, data):
         return zip(*sorted(enumerate(data), key=lambda l: l[1][4]))
 
-    def sort_on_col_6(self, data):
+    def sort_dmged(self, data):
         return zip(*sorted(enumerate(data), key=lambda l: l[1][5]))
 
-    def sort_on_col_7(self, data):
-        return zip(*sorted(enumerate(data), key=lambda l: l[1][6]))
+    def sort_min_price(self, data):
+        return self.money_value_sort(data, 6, True)
+
+    def sort_avg_price(self, data):
+        return self.money_value_sort(data, 7)
+
+    def sort_max_price(self, data):
+        return self.money_value_sort(data, 8)
+
+    def money_value_sort(self, data, column, lower_num=False):
+        """_summary_
+
+        Args:
+            data (tuple): data that will be sorted
+            column (int): number to be sorted
+            lower_num (bool, optional): If True will use the lower value in a range
+                ele will use the higher number. Defaults to False.
+
+        Returns:
+            _type_: _description_
+        """
+        pd_data = pd.DataFrame(data)
+        prices = self.string_prices_to_numbers(pd_data[column])
+        range_column = 0 if lower_num else 1
+        new_index = prices.sort_values(by=range_column).index
+        pd_data = pd_data.loc[new_index]
+        tuple(new_index)
+        data = self.table_format(pd_data)
+        return [tuple(new_index), tuple(data)]
 
     def on_row_press(self, instance_table, instance_row):
         """Called when a table row is clicked."""
--- app/kivy_example.py
+++ app/kivy_example.py
@@ -1,110 +1,36 @@
-import os
-
-os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
-
-import datetime
-
-import cv2
-from kivy.app import App
-from kivy.clock import Clock
-from kivy.graphics.texture import Texture
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
-from kivy.uix.label import Label
+from kivy.metrics import dp
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.screenmanager import MDScreenManager
 
 
-class CameraScreen(MDScreen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.capture = None
-        self.texture = None
-        self.image = None
-        self.timestamp_label = None
-        self.layout = BoxLayout(orientation="vertical")
-        self.add_widget(self.layout)
-
-    def on_enter(self):
-        """Called when the screen is fully entered and displayed."""
-        self.init_capture_device()
-        self.init_ui_elements()
-        # Schedule the update function to run as frequently as possible
-        Clock.schedule_interval(self.update, 0)
-
-    def init_capture_device(self):
-        """Initialize the OpenCV video capture device."""
-        self.capture = cv2.VideoCapture(0)
-        if not self.capture.isOpened():
-            print("Failed to open camera.")
-            exit()
-
-        ret, frame = self.capture.read()
-        if ret:
-            print(f"Frame shape: {frame.shape}")  # Print frame shape
-        else:
-            print("Failed to grab frame from camera.")
-
-    def init_ui_elements(self):
-        """Initialize the UI elements for the application."""
-        ret, frame = self.get_frame_from_capture()
-        if ret:
-            self.texture = self.create_texture_from_frame(frame)
-            # Create Image widget for Kivy to display the texture
-            self.image = Image(texture=self.texture, size_hint_y=0.9)
-            self.layout.add_widget(self.image)
-            # A label to display the timestamp
-            self.timestamp_label = MDLabel(size_hint_y=0.1, font_size=20)
-            self.layout.add_widget(self.timestamp_label)
-        else:
-            print("Failed to get frame for UI initialization.")
-
-    def get_frame_from_capture(self):
-        """Get a frame from the OpenCV video capture and convert it to RGB."""
-        ret, frame = self.capture.read()
-        if ret:
-            frame = cv2.flip(frame, 0)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        return ret, frame
-
-    def create_texture_from_frame(self, frame):
-        """Create a Kivy texture from an RGB frame."""
-        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt="rgb")
-        texture.blit_buffer(frame.tobytes(), colorfmt="rgb", bufferfmt="ubyte")
-        return texture
-
-    def update_texture(self, frame):
-        """Update the Kivy texture with a new RGB frame."""
-        self.texture.blit_buffer(frame.tobytes(), colorfmt="rgb", bufferfmt="ubyte")
-        self.image.texture = self.texture
-
-    def update(self, dt):
-        """Main update loop."""
-        ret, frame = self.get_frame_from_capture()
-        if ret:
-            self.update_texture(frame)
-            self.update_timestamp()
-
-    def update_timestamp(self):
-        """Update the timestamp displayed in the label."""
-        current_time = datetime.datetime.now().strftime("%H:%M:%S.%f")
-        self.timestamp_label.text = f"Updated at: {current_time}"
-
-    def on_stop(self):
-        """Close the video capture when the application stops."""
-        self.capture.release()
-
-
-class CameraApp(MDApp):
+class Example(MDApp):
     def build(self):
-        sm = MDScreenManager()
-        cs = CameraScreen(name="camera_page")
-        sm.add_widget(cs)
-        sm.current = "camera_page"
-        return sm
+        self.data_tables = MDDataTable(
+            use_pagination=True,
+            check=True,
+            column_data=[
+                ("No.", dp(30), self.sort_col_1),
+                ("Team Lead", dp(30), self.sort_col_2),
+            ],
+            row_data=[
+                ("1", "Chase Nguyen"),
+                ("2", "Brie Furman"),
+                ("3", "Jeremy lake"),
+                ("4", "Angelica Howards"),
+                ("5", "Diane Okuma"),
+            ],
+            elevation=2,
+        )
+        screen = MDScreen()
+        screen.add_widget(self.data_tables)
+        return screen
+
+    def sort_col_1(self, data):
+        return zip(*sorted(enumerate(data), key=lambda l: l[1][0]))
+
+    def sort_col_2(self, data):
+        return zip(*sorted(enumerate(data), key=lambda l: l[1][1]))
 
 
-if __name__ == "__main__":
-    CameraApp().run()
+Example().run()
